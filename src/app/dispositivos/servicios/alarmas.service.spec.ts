c003e354ed479af5d03bed9c0425a32cc344161f
Alberto Gonzalez Perez <algonzal@uji.es>
2020-12-20 18:09:15 +0100

    Añadir ejemplo de prueba de error con observable

--- src/app/dispositivos/servicios/alarmas.service.spec.ts
+++ src/app/dispositivos/servicios/alarmas.service.spec.ts
@@ -19,11 +19,12 @@ describe('AlarmasService', () => {
   });
 
   it('debería detectar los saltos de alarma', async () => {
-    const saltosDeAlarma = alarmas.alarmas$.pipe(
+    const saltosDeAlarma = alarmas.alarmas$.pipe( // OJO, escuchamos pero no esperamos
       take(5),
       toArray()
     ).toPromise();
 
+    // Simulamos la llegada de valores por el observable
     topicoEstados.next([
       {id: 0, tipo: TipoDispositivo.SENSOR_MOVIMIENTO, estado: 'quieto'},
       {id: 1, tipo: TipoDispositivo.ALARMA, estado: 'apagada'},
@@ -57,7 +58,7 @@ describe('AlarmasService', () => {
     // y no puede volver a usarse.
     // Tened en cuenta esto a la hora de implementar vuestros observables / subjects.
 
-    const cambios = await saltosDeAlarma;
+    const cambios = await saltosDeAlarma; // Esperamos a que ser recojan los 5 cambios
     expect(cambios[0]).toEqual([]);
     expect(cambios[1]).toEqual([]);
     expect(cambios[2]).toEqual([1]);
@@ -70,8 +71,24 @@ function crearMockServicioDispositivos(): [jasmine.SpyObj<ServicioDispositivos>,
   const topicoEstados = new Subject<Array<Dispositivo>>();
   const mock = jasmine.createSpyObj(
     'DispositivosService',
-    [],
-    {estados$: topicoEstados}
+    [ // Si nuestro servicio a reemplazar tiene métodos, indicar sus nombres aquí
+      /* 'metodoDeEjemplo' */
+    ],
+    {estados$: topicoEstados} // Podemos configurar el valor a devolver por sus propiedades públicas
     );
-  return [mock, topicoEstados];
+
+  // Para configurar el comportamiento de un método (en lugar de una propiedad), haríamos lo siguiente:
+  // Para devolver siempre lo mismo
+  // mock.metodoDeEjemplo.and.returnValue(/* valor a devolver al llamar al método */);
+  // Para devolver algo en función de los argumentos de llamada al método
+  // mock.metodoDeEjemplo.withArgs(/* valor argumentos */)
+  //   .and.returnValue(/* valor a devolver por el método cuando los argumentos coincidan con los indicados en 'valor agumentos'*/);
+
+  // POSTERIORMENTE, en cada prueba, podemos comprobar si el método se ha llamado con:
+  // expect(mock.metodoDeEjemplo).toHaveBeenCalled(); // Se ha llamado
+  // expect(mock.metodoDeEjemplo).toHaveBeenCalledWith(/* Argumentos */); // Se ha llamado con unos argumentos concretos
+  // expect(mock.metodoDeEjemplo).toHaveBeenCalledTimes(/* veces */); // Se ha llamado n veces
+  // expect(mock.metodoDeEjemplo).toHaveBeenCalledOnceWith(/* Argumentos */); // Se ha llamado una vez con unos argumentos concretos
+
+  return [mock, topicoEstados]; // Tupla con el mock y el valor con capacidad de escritura de su propiedad pública
 }
