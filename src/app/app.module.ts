eb9b951467bbc2a7d98344d6e926364a44a5ba7d
Alberto Gonzalez Perez <algonzal@uji.es>
2020-12-20 17:24:30 +0100

    Enlazar interfaz servicio con implementación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SERVICIO_DISPOSITIVOS} from './dispositivos/servicios/i-dispositivos';
+import {DispositivosService} from './dispositivos/servicios/dispositivos.service';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: SERVICIO_DISPOSITIVOS, useClass: DispositivosService}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
