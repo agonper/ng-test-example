95bd0e7eaa41c7035ba586d1fa7ca1b1da3eba6e
Alberto Gonzalez Perez <algonzal@uji.es>
2020-12-20 17:25:16 +0100

    Crear ejemplo de uso de observable en vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AlarmasService} from './dispositivos/servicios/alarmas.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-test-example';
+  alarmas$: Observable<Array<number>>;
+
+  constructor(private servicioAlarmas: AlarmasService) {
+    this.alarmas$ = servicioAlarmas.alarmas$;
+  }
 }
